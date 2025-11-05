7d3076b9e6aaa47bc1465dd9b6269083d053336a
Lorenzo Ronco <115119988+LorenzoRonco@users.noreply.github.com>
2025-11-05 18:46:52 +0100

    DB design: draft of the DB for first 3 stories

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();   // Prisma Client
 
 const app: Express = express();
 const port = 4000;
 
+app.use(express.json());  // middleware for JSON
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+
+// close Prisma connection
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
 });
 
 app.listen(port, () => {
