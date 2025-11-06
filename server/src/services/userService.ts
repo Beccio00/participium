97edbdd5535901bf61c601f094cd3f86043b4eeb
gabbo0501 <gabrielemondino05@gmail.com>
2025-11-06 12:41:41 +0100

    generated moved

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,5 +1,5 @@
 import { PrivateUser } from "../interfaces/User";
-import { PrismaClient } from "../generated/client";
+import { PrismaClient } from "../../prisma/generated/client";
 
 const prisma = new PrismaClient();
 
