ef16390069013360e9a3e098b8e69c5d9fa1c4bb
gabbo0501 <gabrielemondino05@gmail.com>
2025-11-10 21:33:39 +0100

    integration test

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from "../prisma/generated/client";
 export const prisma = new PrismaClient();
 
 const app = createApp();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // close Prisma connection
 process.on("SIGTERM", async () => {
