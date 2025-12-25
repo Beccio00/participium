edf3784d1e054b7926fa35466887aa470798dd94
LORENZO <lollo.ianto@icloud.com>
2025-12-25 16:43:02 +0100

    fix: backend logic full implemented

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -4,9 +4,12 @@ import { login, logout, getSessionInfo } from "../controllers/authController";
 
 const router = Router();
 
-//unregistered users can ee approved reports on an interactive map
+//unregistered users can see approved reports on an interactive map
+
 router.post("/", asyncHandler(login));
+
 router.delete("/current", asyncHandler(logout));
+
 router.get("/current", asyncHandler(getSessionInfo));
 
 export default router;
